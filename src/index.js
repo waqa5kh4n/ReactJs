1e7f3e4a967e28e61367ee36fb1eeae823957a5b
Waqas Khan <me@waqaskhan.dev>
2020-07-14 16:27:49 -0400

    Conditionally rendering

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function SkiResort({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}</h1>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function Lake({ name }) {
+  return (
+    <div>
+      <h1>Visit {name}</h1>
+    </div>
+  );
+}
+
+function App(prop) {
+  return (
+    <div>
+      {prop.season === "summer" ? (
+        <Lake name="Jenny Lake" />
+      ) : prop.season === "winter" ? (
+        <SkiResort name="JHMR" />
+      ) : (
+        <h1>Come back in the winter or summer</h1>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App season="winter" />, document.getElementById("root"));
